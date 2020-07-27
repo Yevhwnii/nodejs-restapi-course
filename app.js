c3cfe67cb23629f71b53e7590b949a8adb43cbce
Yevhwnii <mirandoos3223@gmail.com>
2020-07-27 16:29:11 +0300

    # Initial setup

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
+// Enviroment variable
+require('dotenv').config();
+// 3rd-party imports
 const express = require('express');
-
+const bodyParser = require('body-parser');
+// Local imports
 const feedRoutes = require('./routes/feed');
-
+// Global variables
+const port = process.env.SERVER_PORT;
 const app = express();
-
+// 3rd-party middleware
+app.use(bodyParser.json()); // - since we want to expect json data on both req and res, we parse it thru json method
+// Using routes
 app.use('/feed', feedRoutes);
-
-app.listen(8080);
+// Listening to port
+app.listen(port);
+console.log(`Server is running on port - ${port}`);
