dc5496a2cb3d1695968333968749966e4e642a55
Yevhwnii <mirandoos3223@gmail.com>
2020-07-27 16:28:48 +0300

    # Dummy routes

--- routes/feed.js
+++ routes/feed.js
@@ -5,5 +5,7 @@ const feedController = require('../controllers/feed');
 const router = express.Router();
 // GET => /feed/posts
 router.get('/posts', feedController.getPosts);
+// POST => /feed/post
+router.post('/post', feedController.postPost);
 
 module.exports = router;
