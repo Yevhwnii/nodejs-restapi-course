1490f994e08d3820e3ffcc65780b56cd562cb6c1
Yevhwnii <mirandoos3223@gmail.com>
2020-07-29 20:36:32 +0300

    # post routes + validators

--- routes/feed.js
+++ routes/feed.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { body } = require('express-validator');
 
 const feedController = require('../controllers/feed');
 
@@ -6,6 +7,15 @@ const router = express.Router();
 // GET => /feed/posts
 router.get('/posts', feedController.getPosts);
 // POST => /feed/post
-router.post('/post', feedController.postPost);
+router.post(
+  '/post',
+  [
+    body('title').trim().isLength({ min: 5 }),
+    body('content').trim().isLength({ min: 5 }),
+  ],
+  feedController.postPost
+);
+// GET => /feed/post/postId - single post
+router.get('/post/:postId', feedController.getPost);
 
 module.exports = router;
