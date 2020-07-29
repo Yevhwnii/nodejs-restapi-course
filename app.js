173bfdc388ef84f562a7d529fb5ff8335770bbaa
Yevhwnii <mirandoos3223@gmail.com>
2020-07-29 20:37:23 +0300

    # connection mongodb, multer, error handler

--- app.js
+++ app.js
@@ -1,15 +1,40 @@
 // Enviroment variable
 require('dotenv').config();
 // 3rd-party imports
+const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const multer = require('multer');
 // Local imports
 const feedRoutes = require('./routes/feed');
 // Global variables
 const port = process.env.SERVER_PORT;
 const app = express();
+// Multer configuration
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'images');
+  },
+  filename: (req, file, cb) => {
+    cb(null, Math.random() + '-' + file.originalname);
+  },
+});
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === 'image/png' ||
+    file.mimetype === 'image/jpg' ||
+    file.mimetype === 'image/jpeg'
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
 // 3rd-party middleware
+app.use(multer({ storage: fileStorage }, fileFilter).single('image'));
 app.use(bodyParser.json()); // - since we want to expect json data on both req and res, we parse it thru json method
+app.use('/images', express.static(path.join(__dirname, 'images')));
 // Handling CORS errors
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -19,6 +44,20 @@ app.use((req, res, next) => {
 });
 // Using routes
 app.use('/feed', feedRoutes);
-// Listening to port
-app.listen(port);
-console.log(`Server is running on port - ${port}`);
+// Global error handler
+app.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  res.status(status).json({
+    message,
+  });
+});
+// Establishing connection with database and running server
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then((res) => {
+    app.listen(port);
+    console.log(`Server is running on port - ${port}`);
+  })
+  .catch((err) => console.log(err));
