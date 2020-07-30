e0630e2f0f482ed3fd50cc0de2a1553712855fd6
Yevhwnii <mirandoos3223@gmail.com>
2020-07-30 14:27:09 +0300

    # auth controller

---
+++ controllers/auth.js
@@ -0,0 +1,82 @@
+require('dotenv').config();
+
+const { validationResult } = require('express-validator');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const User = require('../models/user');
+
+const SECRET_KEY = process.env.JWT_SECRET_KEY;
+
+exports.signup = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const error = new Error('Validation failed');
+    error.statusCode = 422;
+    error.data = errors.array();
+    throw error;
+  }
+  const email = req.body.email;
+  const password = req.body.password;
+  const name = req.body.name;
+  bcrypt
+    .hash(password, 12)
+    .then((hashedPassword) => {
+      const user = new User({
+        email,
+        password: hashedPassword,
+        name,
+      });
+      return user.save();
+    })
+    .then((result) => {
+      res.status(201).json({
+        message: 'User was created',
+        userId: result._id,
+      });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
+
+exports.login = (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  let loadedUser;
+  User.findOne({ email: email })
+    .then((user) => {
+      if (!user) {
+        const error = new Error('User with this email is not found');
+        error.statusCode = 401;
+        throw error;
+      }
+      loadedUser = user;
+      return bcrypt.compare(password, user.password);
+    })
+    .then((isEqual) => {
+      if (!isEqual) {
+        const error = new Error('Wrong passowrd');
+        error.statusCode = 401;
+        throw error;
+      }
+      const token = jwt.sign(
+        { email: loadedUser.email, userId: loadedUser._id.toString() },
+        SECRET_KEY,
+        { expiresIn: '1h' }
+      );
+      res.status(200).json({
+        token,
+        userId: loadedUser._id.toString(),
+      });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
