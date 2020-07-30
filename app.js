8de714fac7e470ab465499af5f5d351e52860bda
Yevhwnii <mirandoos3223@gmail.com>
2020-07-30 14:27:02 +0300

    # using auth routes

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const mongoose = require('mongoose');
 const multer = require('multer');
 // Local imports
 const feedRoutes = require('./routes/feed');
+const authRoutes = require('./routes/auth');
 // Global variables
 const port = process.env.SERVER_PORT;
 const app = express();
@@ -38,19 +39,22 @@ app.use('/images', express.static(path.join(__dirname, 'images')));
 // Handling CORS errors
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
 });
 // Using routes
 app.use('/feed', feedRoutes);
+app.use('/auth', authRoutes);
 // Global error handler
 app.use((error, req, res, next) => {
   console.log(error);
   const status = error.statusCode || 500;
   const message = error.message;
+  const data = error.data;
   res.status(status).json({
     message,
+    data,
   });
 });
 // Establishing connection with database and running server
